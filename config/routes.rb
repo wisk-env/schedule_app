849822001681776b733e4f0ceb0bd51e2754f707
kfor-code <wiskenv566@gmail.com>
2023-05-07 19:51:37 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'schedules/index'
+  resources :schedules
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
