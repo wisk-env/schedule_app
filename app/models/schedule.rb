b86c309c532b266b1c814b0c9742a0651b7447d0
kfor-code <wiskenv566@gmail.com>
2023-05-07 21:15:37 +0900

    必須項目のカラムと文字数制限のあるカラムにバリデーションを設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :memo, length: { maximum: 500 }
 end
