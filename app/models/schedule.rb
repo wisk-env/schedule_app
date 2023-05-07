d7151a6489aecb7f267ad7bcc2f9b3b5c99ea83c
kfor-code <wiskenv566@gmail.com>
2023-05-07 22:45:00 +0900

    予定の終了日のカラムに過去の日付を使用できないようバリデーションを設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,13 @@ class Schedule < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :memo, length: { maximum: 500 }
+  validate :end_date_cannot_be_in_the_past
+
+  private
+
+  def end_date_cannot_be_in_the_past
+    if end_date.present? && end_date < Date.today
+      errors.add(:end_date, "に過去の日付は使えません")
+    end
+  end
 end
